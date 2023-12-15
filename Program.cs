54a1fcc7c4b9e516ff023ca43f5018dbd87dc1a0
alijonizabiri <alijonzabirov20@mail.ru>
2023-12-15 16:31:20 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+//int a, b;
+//cin >> a >> b;
+//cout << a + b;
+
+int a;
+double b;
+
+a =  Convert.ToInt32(Console.ReadLine());
+b = Convert.ToDouble(Console.ReadLine());
+
+System.Console.WriteLine(a + b);
